d90bbbd5f4ede4995d3ecc29f5eb7b950cd9037d
Carla Mariana A. Braga <carlamab2808@outlook.com>
2022-04-11 19:28:35 -0300

    Create PrimeiraListaEx01.cpp

---
+++ PrimeiraListaEx01.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+
+/*Desenvolva um programa que escreve na tela a frase: Hello World C++*/
+
+int main()
+{
+	std::cout << "Hello World!!" << std::endl;
+	system("PAUSE");
+	return 0;
+}
