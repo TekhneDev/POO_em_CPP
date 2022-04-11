dc1aed22720e8a110df512459c002ea74d1a2945
Carla Mariana A. Braga <carlamab2808@outlook.com>
2022-04-11 19:26:41 -0300

    Create primeiro.cpp

---
+++ primeiro.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+//a diretiva de pré-processador #include indica para incluir 
+//o arquivo de cabeçalho iostream
+
+int main()
+  //função principal. Todo Programa C++ com essa função principal main
+{
+  //cout comando de saída de dados, neste caso está solicitando para colocar
+  //na saída padrão (tela) a frase especificada.
+	std::cout << "Primeiro Programa C++" << std::endl; //endl = \n
+	system("PAUSE");
+	return 0;
+
+}
+//Crtl + K + C = Comentar o código selecionado 
+
+/*Para deixar o console com fundo preto e letras verde clara faça:
+system("COLOR 0D")*/
