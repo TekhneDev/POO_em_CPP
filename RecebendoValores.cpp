304d1c8d324afc1b81d508cd06a9cd8f1a7e5498
Carla Mariana A. Braga <carlamab2808@outlook.com>
2022-04-14 01:28:59 -0300

    Create RecebendoValores.cpp

---
+++ RecebendoValores.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+int main(){
+  
+      int numero01, numero02;
+  std::cout << "Digite o primeiro numero: ";
+  std::cin >> numero01;
+  std::cout <<"numero01 = " << numero01 << std::endl;
+  std::cout <<Digite o segundo numero: ";
+  std::cin >> numero02:
+  std::cout << "numero02 = " << numero02;
+  system("PAUSE");
+}
